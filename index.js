e037862918a0fea6b3ff2ba74f4c6a084804aded
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-02-03 08:26:40 -0300

    (AuthPags): Learning Three.js properties about transformations: Position, Scale, Rotation, GroupClass, and AxesHelper

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 
 // 1. Scene . 2 Mesh(Obejects) 3. Camera 4. Renderer
 
-
-
-
 // Scene
 const scene = new THREE.Scene(); 
 
 // Mesh
 const geometry = new THREE.BoxGeometry(1, 1, 1); 
-const material = new THREE.MeshBasicMaterial({color: "purple"});
+const material = new THREE.MeshBasicMaterial({color: "yellow"});
 const mesh = new THREE.Mesh(geometry, material);
 
-scene.add(mesh);
+// Position (X , Y, Z)
+// mesh.position.x = 1;
+// mesh.position.y = -0.5;
+// mesh.position.z = 1;
+
+// Scale
+// mesh.scale.x = 0.5;
+// mesh.scale.y = 1.7; 
+// mesh.scale.set(0.5 , 1.7);  
+
+// Rotation 
+// mesh.rotation.x = Math.PI * 0.25;
+
+// scene.add(mesh);
+
+const geometryT = new THREE.BoxGeometry(1, 1, 1); 
+const materialT = new THREE.MeshBasicMaterial({color: "blue"});
+const meshT = new THREE.Mesh(geometryT, materialT);
+
+// Posicionando a meshT
+meshT.position.y = 2;
+
+// Criando um grupo e adicionando meshT
+const Group = new THREE.Group();
+Group.add(mesh, meshT);
+Group.position.x = 1;
+Group.position.y = -1;
+
+// Adicionando o grupo à cena
+scene.add(Group);
+
+// scene.add(meshT);
+
+
+// AxesHelper
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper); 
+
 
 
 // Camera
@@ -22,9 +56,12 @@ const aspect = {
 }
 
 const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height ) // near value is 1 , and for value is 2000
+
+
+// Position (X , Y, Z)
 camera.position.z = 4; 
-camera.position.x = 1;
-camera.position.y = -0.5;
+
+
 
 scene.add(camera)
 
